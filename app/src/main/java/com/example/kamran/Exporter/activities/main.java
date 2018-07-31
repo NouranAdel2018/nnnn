6ae6813b64b3d08b635ee84447766d94e281cbb5
Nouran Adel <nouran.adelcis92@gmail.com>
2018-07-31 09:53:37 +0200

    Project

---
+++ app/src/main/java/com/example/kamran/Exporter/activities/main.java
@@ -0,0 +1,41 @@
+package com.example.kamran.Exporter.activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.kamran.Exporter.R;
+
+public class main extends AppCompatActivity {
+    TextView sin;
+    LinearLayout circle;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        circle = (LinearLayout)findViewById(R.id.circle);
+        sin = (TextView)findViewById(R.id.sin);
+
+        circle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent it = new Intent(main.this,RegisterActivity.class);
+                startActivity(it);
+
+            }
+        });
+        sin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(main.this,LoginActivity.class);
+                startActivity(it);
+            }
+        });
+
+    }
+}
